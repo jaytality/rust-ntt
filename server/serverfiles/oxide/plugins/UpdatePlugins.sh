d66c718722222e343c5405a26f41a9f2b3b3f8aa
jaytality <johnathan.tiong@gmail.com>
2023-07-07 17:23:09 +1000

    added plugins

---
+++ server/serverfiles/oxide/plugins/UpdatePlugins.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+wget https://umod.org/plugins/AdminRadar.cs -q --show-progress -O AdminRadar.cs
+sleep 5
+wget https://umod.org/plugins/AntiOfflineRaid.cs -q --show-progress -O AntiOfflineRaid.cs
+sleep 5
+wget https://umod.org/plugins/BetterLoot.cs -q --show-progress -O BetterLoot.cs
+sleep 5
+wget https://umod.org/plugins/ChatClear.cs -q --show-progress -O ChatClear.cs
+sleep 5
+wget https://umod.org/plugins/CustomChatCommands.cs -q --show-progress -O CustomChatCommands.cs
+sleep 5
+wget https://umod.org/plugins/DeathNotes.cs -q --show-progress -O DeathNotes.cs
+sleep 5
+wget https://umod.org/plugins/GatherManager.cs -q --show-progress -O GatherManager.cs
+sleep 5
+wget https://umod.org/plugins/Heal.cs -q --show-progress -O Heal.cs
+sleep 5
+wget https://umod.org/plugins/NightVision.cs -q --show-progress -O NightVision.cs
+sleep 5
+wget https://umod.org/plugins/NoGiveNotices.cs -q --show-progress -O NoGiveNotices.cs
+sleep 5
+wget https://umod.org/plugins/NoGreen.cs -q --show-progress -O NoGreen.cs
+sleep 5
+wget https://umod.org/plugins/PlayerList.cs -q --show-progress -O PlayerList.cs
+sleep 5
+wget https://umod.org/plugins/Prod.cs -q --show-progress -O Prod.cs
+sleep 5
+wget https://umod.org/plugins/QuickSmelt.cs -q --show-progress -O QuickSmelt.cs
+sleep 5
+wget https://umod.org/plugins/RecyclerSpeed.cs -q --show-progress -O RecyclerSpeed.cs
+sleep 5
+wget https://umod.org/plugins/RemoverTool.cs -q --show-progress -O RemoverTool.cs
+sleep 5
+wget https://umod.org/plugins/Rustcord.cs -q --show-progress -O Rustcord.cs
+sleep 5
+wget https://umod.org/plugins/Skins.cs -q --show-progress -O Skins.cs
+sleep 5
+wget https://umod.org/plugins/StackSizeController.cs -q --show-progress -O StackSizeController.cs
+sleep 5
+wget https://umod.org/plugins/TimeOfDay.cs -q --show-progress -O TimeOfDay.cs
+sleep 5
+wget https://umod.org/plugins/Vanish.cs -q --show-progress -O Vanish.cs
+sleep 5
+wget https://umod.org/plugins/Welcomer.cs -q --show-progress -O Welcomer.cs
