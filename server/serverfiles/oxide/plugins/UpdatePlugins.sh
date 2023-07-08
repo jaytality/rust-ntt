f1d69bee9c0680fa8bee7ec2e56b81c38ddbf2dd
jaytality <johnathan.tiong@gmail.com>
2023-07-08 16:52:49 +1000

    Updated plugin update shell script

--- server/serverfiles/oxide/plugins/UpdatePlugins.sh
+++ server/serverfiles/oxide/plugins/UpdatePlugins.sh
@@ -2,18 +2,18 @@
 
 wget https://umod.org/plugins/AdminRadar.cs -q --show-progress -O AdminRadar.cs
 sleep 5
-wget https://umod.org/plugins/AntiOfflineRaid.cs -q --show-progress -O AntiOfflineRaid.cs
-sleep 5
-wget https://umod.org/plugins/BetterLoot.cs -q --show-progress -O BetterLoot.cs
-sleep 5
+#wget https://umod.org/plugins/AntiOfflineRaid.cs -q --show-progress -O AntiOfflineRaid.cs
+#sleep 5
+#wget https://umod.org/plugins/BetterLoot.cs -q --show-progress -O BetterLoot.cs
+#sleep 5
 wget https://umod.org/plugins/ChatClear.cs -q --show-progress -O ChatClear.cs
 sleep 5
 wget https://umod.org/plugins/CustomChatCommands.cs -q --show-progress -O CustomChatCommands.cs
 sleep 5
 wget https://umod.org/plugins/DeathNotes.cs -q --show-progress -O DeathNotes.cs
 sleep 5
-wget https://umod.org/plugins/GatherManager.cs -q --show-progress -O GatherManager.cs
-sleep 5
+#wget https://umod.org/plugins/GatherManager.cs -q --show-progress -O GatherManager.cs
+#sleep 5
 wget https://umod.org/plugins/Heal.cs -q --show-progress -O Heal.cs
 sleep 5
 wget https://umod.org/plugins/NightVision.cs -q --show-progress -O NightVision.cs
@@ -26,18 +26,18 @@ wget https://umod.org/plugins/PlayerList.cs -q --show-progress -O PlayerList.cs
 sleep 5
 wget https://umod.org/plugins/Prod.cs -q --show-progress -O Prod.cs
 sleep 5
-wget https://umod.org/plugins/QuickSmelt.cs -q --show-progress -O QuickSmelt.cs
-sleep 5
-wget https://umod.org/plugins/RecyclerSpeed.cs -q --show-progress -O RecyclerSpeed.cs
-sleep 5
+#wget https://umod.org/plugins/QuickSmelt.cs -q --show-progress -O QuickSmelt.cs
+#sleep 5
+#wget https://umod.org/plugins/RecyclerSpeed.cs -q --show-progress -O RecyclerSpeed.cs
+#sleep 5
 wget https://umod.org/plugins/RemoverTool.cs -q --show-progress -O RemoverTool.cs
 sleep 5
-wget https://umod.org/plugins/Rustcord.cs -q --show-progress -O Rustcord.cs
-sleep 5
+#wget https://umod.org/plugins/Rustcord.cs -q --show-progress -O Rustcord.cs
+#sleep 5
 wget https://umod.org/plugins/Skins.cs -q --show-progress -O Skins.cs
 sleep 5
-wget https://umod.org/plugins/StackSizeController.cs -q --show-progress -O StackSizeController.cs
-sleep 5
+#wget https://umod.org/plugins/StackSizeController.cs -q --show-progress -O StackSizeController.cs
+#sleep 5
 wget https://umod.org/plugins/TimeOfDay.cs -q --show-progress -O TimeOfDay.cs
 sleep 5
 wget https://umod.org/plugins/Vanish.cs -q --show-progress -O Vanish.cs
